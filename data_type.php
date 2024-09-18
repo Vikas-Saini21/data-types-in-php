686b5b547032162a801ab60f506ccf3385b004d2
Vikas Saini <154893113+Vikas-Saini21@users.noreply.github.com>
2024-09-18 15:34:32 +0530

    Data_Type.php

---
+++ data_type.php
@@ -0,0 +1,45 @@
+<!-- int data type -->
+<?php
+$value=25;
+var_dump($value)
+?>
+
+
+<!--string data type -->
+<?php
+$value="vikas";
+var_dump($value)
+?>
+
+
+<!-- bool data type -->
+<?php
+$value=true;
+var_dump($value)
+?>
+
+<?php
+$value=false;
+var_dump($value)
+?>
+
+
+<!-- float data type -->
+<?php
+$value=25.5;
+var_dump($value)
+?>
+
+
+<!-- null data type -->
+<?php
+$value=null;
+var_dump($value)
+?>
+
+
+<!-- array data type -->
+<?php
+$value=array(1,2,3,4);
+var_dump($value)
+?>
